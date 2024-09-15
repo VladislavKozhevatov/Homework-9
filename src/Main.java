06ffdc0e0e152aa5d499523bda547115b3faf3ff
Vladislav <tekuzar@mail,ru>
2024-09-15 19:29:44 +0300

    Init!

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,5 +38,20 @@ public class Main {
         }
         wastesPerMonth = sum1 /4;//делим на 4 недели в месяце.
         System.out.printf("Средняя сумма трат за месяц составила %.2f рублей%n " ,wastesPerMonth);
+
+        //Задача 4
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i>=0; i--) {
+            System.out.println(reverseFullName[i]);
+        }
+        System.out.println();
+
+        for (int i=0; i<reverseFullName.length/2; i++){
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length-i-1];
+            reverseFullName[reverseFullName.length-i-1]=temp;
+        }
+        System.out.print(Arrays.toString(reverseFullName));
     }
 }
